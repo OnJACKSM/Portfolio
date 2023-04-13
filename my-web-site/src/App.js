6e5a2ae33063278d415feb1241e41d6d9f535862
OnJACK <onjacksm@gmail.com>
2023-04-13 19:27:15 -0300

    feat: Create header component

--- my-web-site/src/App.js
+++ my-web-site/src/App.js
@@ -1,9 +1,15 @@
 import './styles.css';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div className="App">
       <header>
+        {/* header */}
+        <Header />
+        {/* main */}
+
+        {/* sections */}
         Hello World
       </header>
     </div>
---
+++ my-web-site/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <img
+                    src="https://github.com/onjacksm.png"
+                    alt="Foto de Jackson com Óculos"
+                />
+            </div>
+        </header>
+    )
+}
